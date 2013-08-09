730eb4ede7b5ba342b517972f4c6746a937883c6
kim hyung joo <kimhyungjoo@kim-ui-MacBook-Pro.local>
2013-08-09 23:05:19 +0900

    Initial Commit

---
+++ gittest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  gittest
+//
+//  Created by kim hyung joo on 13. 8. 9..
+//  Copyright kim hyung joo 2013년. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
